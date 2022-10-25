5d8041d8cb62a59f002bc4f0a80dab1b41aff59e
Zela2520 <sugarbeatzela@gmail.com>
2022-10-25 16:35:53 +0300

    check has been fixed

--- linters/run.sh
+++ linters/run.sh
@@ -11,6 +11,6 @@ print_header "RUN cppcheck"
 cppcheck matrix_lib/matrix/*.cpp matrix_lib/main.c --enable=all --inconclusive -I matrix_lib/matrix --suppress=missingIncludeSystem
 
 print_header "RUN clang-tidy"
-clang-tidy matrix_lib/matrix/*.cpp matrix_lib/main.cpp -- -std=c++20 -Imatrix_lib/matrix
+clang-tidy matrix_lib/matrix/*.cpp matrix_lib/main.cpp -- -std=c++20 -Imatrix_lib/matrix -Imatrix_lib/matrix/matrixRow -Imatrix_lib/matrix/matrixSimple -Imatrix_lib/matrix/matrixCol
 
 print_header "SUCCESS"
